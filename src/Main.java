74e60b3d069c595e8cedf8c4ce04a60454c59ec2
francesco-mauro <francesco.mauro180@gmail.com>
2025-10-05 16:49:08 +0200

    Library

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,28 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        int newPrice;
 
-        Car car = new Car("Charger", "Yellow", 10000);
+//        String infoTitle;
+//        int infoPages;
+//
+//        System.out.println("Inserisci il titolo del libro");
+//        infoTitle = scanner.nextLine();
+//
+//        System.out.println("Inserisci il numero di pagine del libro");
+//        infoPages = scanner.nextInt();
+
+        Book book1 = new Book("The Fellowship of the ring", 423);
+        Book book2 = new Book("The two Towers", 354);
+        Book book3 = new Book("The return of the King", 416);
+
+        Book[] books = {book1, book2, book3};
 
 
-        car.setColor("Blue");
-        System.out.println("Input new price");
-        newPrice = scanner.nextInt();
-        car.setPrice(newPrice);
 
-        System.out.println(car.getColor() +" "+ car.getModel() +" "+ car.getPrice());
+        for (Book book: books){
+            System.out.println(book.displayInfo());
+        }
+
 
         scanner.close();
     }
